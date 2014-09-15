d38994c5a6216aa56416b05408b1816c9369c63f
jfabris26 <jack.fabris@gmail.com>
2014-09-14 21:34:04 -0400

    Initial commit

---
+++ GeminiCore/CPU.cs
@@ -0,0 +1,37 @@
+﻿/**
+ * Seth Morecraft
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeminiCore
+{
+    public class CPU
+    {
+        public int A { get; private set; }
+        public int B { get; private set; }
+        public int ACC { get; private set; }
+        public int ZERO { get; private set; }
+        public int ONE { get; private set; }
+        public int PC { get; private set; }
+        public int MAR { get; private set; }
+        public int MDR { get; private set; }
+        public int TEMP { get; private set; }
+        public int IR { get; private set; }
+        public int CC { get; private set; }
+
+
+        public CPU()
+        {
+            ACC = 0;
+        }
+
+        public void nextInstruction()
+        {
+            ACC++;
+        }
+    }
+}
---
+++ GeminiCore/IPE.cs
@@ -0,0 +1,49 @@
+﻿/**
+ * Seth Morecraft
+ */
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace GeminiCore
+{
+    public class IPE
+    {
+        public string FileToParse { get; set; }
+
+        public IPE(string filename)
+        {
+            this.FileToParse = filename;
+        }
+
+        public void ParseFile()
+        {
+            var lines = File.ReadAllLines(this.FileToParse).ToList<string>();
+            var count = 0;
+            foreach (var line in lines)
+            {
+                Regex labelStmtFormat = new Regex(@"^(?<label>.*?)\s*:$");
+                Regex emptyLine = new Regex(@"^\s*$");
+                Regex comment = new Regex(@"!(.*)$");
+                var emptyMatch = emptyLine.Match(line);
+                var labelStmtMatch = labelStmtFormat.Match(line);
+                if (comment.Match(line).Success) { 
+                    count++;
+                    Console.WriteLine(line);
+                    comment.Replace(line, "");
+                    Console.WriteLine(line);
+                }
+                if (labelStmtMatch.Success)
+                {
+                   var label = labelStmtMatch.Groups["label"].Value;
+                   Console.WriteLine(label);
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
